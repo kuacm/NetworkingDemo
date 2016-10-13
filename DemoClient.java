a753de0d03d2e71ef43bc67e68c224ec68e69919
Luigi Vincent <luigipvincent@gmail.com>
2016-10-13 14:08:20 -0400

    Initial commit

---
+++ DemoClient.java
@@ -0,0 +1,19 @@
+/* Author: Luigi Vincent
+* ACM Networking workshop demo client
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DemoClient {
+	private final static String ADDRESS = "localhost";
+	private final static int PORT = 9001;
+
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
---
+++ DemoServer.java
@@ -0,0 +1,20 @@
+/* Author: Luigi Vincent
+* ACM Networking workshop demo server
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class DemoServer {
+	private final static int PORT = 9001;
+
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
